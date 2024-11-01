cf2fde533a002605b4ee41f45f414be140548252
huydq2152 <huy.dangquang@euroland.com>
2024-11-01 10:37:36 +0700

    add conceptual AbstractFactory design pattern

---
+++ AbstractFactory/Conceptual/Program.cs
@@ -0,0 +1,118 @@
+﻿interface IAbstractFactory
+{
+    IAbstractProductA CreateProductA();
+    IAbstractProductB CreateProductB();
+}
+
+class ConcreteFactory1 : IAbstractFactory
+{
+    public IAbstractProductA CreateProductA()
+    {
+        return new ConcreteProductA1();
+    }
+
+    public IAbstractProductB CreateProductB()
+    {
+        return new ConcreteProductB1();
+    }
+}
+
+class ConcreteFactory2 : IAbstractFactory
+{
+    public IAbstractProductA CreateProductA()
+    {
+        return new ConcreteProductA2();
+    }
+
+    public IAbstractProductB CreateProductB()
+    {
+        return new ConcreteProductB2();
+    }
+}
+
+interface IAbstractProductA
+{
+    string UsefulFunctionA();
+}
+
+interface IAbstractProductB
+{
+    string UsefulFunctionB();
+    string AnotherUsefulFunctionB(IAbstractProductA collaborator);
+}
+
+class ConcreteProductA1 : IAbstractProductA
+{
+    public string UsefulFunctionA()
+    {
+        return "Product A1";
+    }
+}
+
+class ConcreteProductA2 : IAbstractProductA
+{
+    public string UsefulFunctionA()
+    {
+        return "Product A2";
+    }
+}
+
+class ConcreteProductB1 : IAbstractProductB
+{
+    public string UsefulFunctionB()
+    {
+        return "Product B1";
+    }
+
+    public string AnotherUsefulFunctionB(IAbstractProductA collaborator)
+    {
+        var result = collaborator.UsefulFunctionA();
+
+        return $"Product B1 collaborating with ({result})";
+    }
+}
+
+class ConcreteProductB2 : IAbstractProductB
+{
+    public string UsefulFunctionB()
+    {
+        return "Product B2";
+    }
+
+    public string AnotherUsefulFunctionB(IAbstractProductA collaborator)
+    {
+        var result = collaborator.UsefulFunctionA();
+
+        return $"Product B2 collaborating with ({result})";
+    }
+}
+
+class Client
+{
+    public void Main()
+    {
+        Console.WriteLine("Client: Testing client code with the first factory type...");
+        ClientMethod(new ConcreteFactory1());
+        Console.WriteLine();
+
+        Console.WriteLine("Client: Testing the same client code with the second factory type...");
+        ClientMethod(new ConcreteFactory2());
+    }
+
+    private void ClientMethod(IAbstractFactory factory)
+    {
+        var productA = factory.CreateProductA();
+        var productB = factory.CreateProductB();
+
+        Console.WriteLine(productB.UsefulFunctionB());
+        Console.WriteLine(productB.AnotherUsefulFunctionB(productA));
+    }
+}
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        new Client().Main();
+    }
+}
