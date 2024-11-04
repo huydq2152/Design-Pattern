c5ba4be23ae53c99a6c5e7c329ae2056ff119941
huydq2152 <quanghuy2152@gmail.com>
2024-11-04 22:34:10 +0700

    add conceptual decorator design pattern

---
+++ Decorator/Conceptual/Program.cs
@@ -0,0 +1,88 @@
+﻿public abstract class Component
+{
+    public abstract string Operation();
+}
+
+public class ConcreteComponent : Component
+{
+    public override string Operation()
+    {
+        return "ConcreteComponent";
+    }
+}
+
+public abstract class Decorator: Component
+{
+    private Component? _component;
+
+    protected Decorator(Component component)
+    {
+        _component = component;
+    }
+
+    public void SetComponent(Component component)
+    {
+        _component = component;
+    }
+    
+    public override string Operation()
+    {
+        if (_component != null)
+        {
+            return _component.Operation();
+        }
+        else
+        {
+            return string.Empty;
+        }
+    }
+}
+
+class ConcreteDecoratorA : Decorator
+{
+    public ConcreteDecoratorA(Component comp) : base(comp)
+    {
+    }
+    public override string Operation()
+    {
+        return $"ConcreteDecoratorA({base.Operation()})";
+    }
+}
+
+class ConcreteDecoratorB : Decorator
+{
+    public ConcreteDecoratorB(Component comp) : base(comp)
+    {
+    }
+
+    public override string Operation()
+    {
+        return $"ConcreteDecoratorB({base.Operation()})";
+    }
+}
+
+public class Client
+{
+    public void ClientCode(Component component)
+    {
+        Console.WriteLine("RESULT: " + component.Operation());
+    }
+}
+    
+public static class Program
+{
+    static void Main(string[] args)
+    {
+        Client client = new Client();
+
+        var simple = new ConcreteComponent();
+        Console.WriteLine("Client: I get a simple component:");
+        client.ClientCode(simple);
+        Console.WriteLine();
+        
+        ConcreteDecoratorA decorator1 = new ConcreteDecoratorA(simple);
+        ConcreteDecoratorB decorator2 = new ConcreteDecoratorB(decorator1);
+        Console.WriteLine("Client: Now I've got a decorated component:");
+        client.ClientCode(decorator2);
+    }
+}
