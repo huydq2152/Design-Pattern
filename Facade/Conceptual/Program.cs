b976c9c16a2b9f7f9a0dead2a7a9e727ecef12aa
huydq2152 <quanghuy2152@gmail.com>
2024-11-04 23:14:14 +0700

    add facade design pattern

---
+++ Facade/Conceptual/Program.cs
@@ -0,0 +1,67 @@
+﻿public class Facade
+{
+    private readonly Subsystem1 _subsystem1;
+    private readonly Subsystem2 _subsystem2;
+
+    public Facade(Subsystem1 subsystem1, Subsystem2 subsystem2)
+    {
+        _subsystem1 = subsystem1;
+        _subsystem2 = subsystem2;
+    }
+    
+    public string Operation()
+    {
+        string result = "Facade initializes subsystems:\n";
+        result += _subsystem1.Operation1();
+        result += _subsystem2.Operation1();
+        result += "Facade orders subsystems to perform the action:\n";
+        result += _subsystem1.OperationN();
+        result += _subsystem2.OperationZ();
+        return result;
+    }
+}
+
+public class Subsystem1
+{
+    public string Operation1()
+    {
+        return "Subsystem1: Ready!\n";
+    }
+
+    public string OperationN()
+    {
+        return "Subsystem1: Go!\n";
+    }
+}
+    
+public class Subsystem2
+{
+    public string Operation1()
+    {
+        return "Subsystem2: Get ready!\n";
+    }
+
+    public string OperationZ()
+    {
+        return "Subsystem2: Fire!\n";
+    }
+}
+
+public static class Client
+{
+    public static void ClientCode(Facade facade)
+    {
+        Console.Write(facade.Operation());
+    }
+}
+    
+public static class Program
+{
+    static void Main(string[] args)
+    {
+        Subsystem1 subsystem1 = new Subsystem1();
+        Subsystem2 subsystem2 = new Subsystem2();
+        Facade facade = new Facade(subsystem1, subsystem2);
+        Client.ClientCode(facade);
+    }
+}
---
+++ Facade/ShopFacade/Program.cs
@@ -0,0 +1,114 @@
+﻿namespace ShopFacade;
+
+public class LoginService
+{
+    public void LoginFb(string userName)
+    {
+        Console.WriteLine(userName + "Login Facebook");
+    }
+
+    public void LoginYoutube(string userName)
+    {
+        Console.WriteLine(userName + "Login Youtube");
+    }
+
+    public void LoginGithub(string userName)
+    {
+        Console.WriteLine(userName + "Login Github");
+    }
+}
+
+public class PaymentService
+{
+    public void PayByPayPal()
+    {
+        Console.WriteLine("Pay by PayPal");
+    }
+
+    public void PayByMoMo()
+    {
+        Console.WriteLine("Pay by MoMo");
+    }
+
+    public void PayByDebitCard()
+    {
+        Console.WriteLine("Pay by debit card");
+    }
+}
+
+public class ShipService
+{
+    public void NowShip()
+    {
+        Console.WriteLine("Now Ship");
+    }
+
+    public void GrabShip()
+    {
+        Console.WriteLine("Grab Ship");
+    }
+
+    public void NinjaVanShip()
+    {
+        Console.WriteLine("Ninja Van Ship");
+    }
+}
+
+public class NotificationService
+{
+    public void NotifyViaSms()
+    {
+        Console.WriteLine("Notification SMS");
+    }
+
+    public void NotifyViaSmsGmail()
+    {
+        Console.WriteLine("Notification Gmail");
+    }
+}
+
+public class ShopFacade
+{
+    private readonly LoginService _loginService;
+    private readonly PaymentService _paymentService;
+    private readonly ShipService _shipService;
+    private readonly NotificationService _notificationService;
+
+
+    public ShopFacade(LoginService loginService, PaymentService paymentService, ShipService shipService,
+        NotificationService notificationService)
+    {
+        _loginService = loginService;
+        _paymentService = paymentService;
+        _shipService = shipService;
+        _notificationService = notificationService;
+    }
+
+    public void BuyProductByMoMoWithGrabShipping(string name)
+    {
+        _loginService.LoginFb(name);
+        _paymentService.PayByMoMo();
+        _shipService.GrabShip();
+        _notificationService.NotifyViaSms();
+    }
+
+    public void BuyProductByPayPalWithNinjaVanShipping(string name)
+    {
+        _loginService.LoginGithub(name);
+        _paymentService.PayByPayPal();
+        _shipService.NinjaVanShip();
+        _notificationService.NotifyViaSmsGmail();
+    }
+}
+
+public static class Program
+{
+    public static void Main()
+    {
+        var shopFacade = new ShopFacade(new LoginService(), new PaymentService(), new ShipService(),
+            new NotificationService());
+        shopFacade.BuyProductByMoMoWithGrabShipping("Nick");
+        Console.WriteLine("----------------------------------------");
+        shopFacade.BuyProductByPayPalWithNinjaVanShipping("Nick");
+    }
+}
