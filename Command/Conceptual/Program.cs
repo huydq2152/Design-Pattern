724f39e2d2dccff21c6d31a50824e06791921805
huydq2152 <huy.dangquang@euroland.com>
2024-11-06 16:05:39 +0700

    add command design pattern

---
+++ Command/Conceptual/Program.cs
@@ -0,0 +1,101 @@
+﻿public class Receiver
+{
+    public void DoA(string a)
+    {
+        Console.WriteLine($"Receiver: Working on ({a}.)");
+    }
+
+    public void DoB(string b)
+    {
+        Console.WriteLine($"Receiver: Working on ({b}.)");
+    }
+}
+
+public interface ICommand
+{
+    void Execute();
+}
+
+public class SimpleCommand : ICommand
+{
+    private readonly string _payload;
+
+    public SimpleCommand(string payload)
+    {
+        _payload = payload;
+    }
+
+    public void Execute()
+    {
+        Console.WriteLine($"SimpleCommand: See, I can do simple things like printing ({_payload})");
+    }
+}
+
+public class ComplexCommand : ICommand
+{
+    private readonly Receiver _receiver;
+    private readonly string _a;
+    private readonly string _b;
+
+    public ComplexCommand(Receiver receiver, string a, string b)
+    {
+        _receiver = receiver;
+        _a = a;
+        _b = b;
+    }
+
+    public void Execute()
+    {
+        Console.WriteLine("ComplexCommand: Complex stuff should be done by a receiver object.");
+        _receiver.DoA(_a);
+        _receiver.DoB(_b);
+    }
+}
+
+public class Invoker
+{
+    private ICommand _onStart;
+
+    private ICommand _onFinish;
+
+    public void SetOnStart(ICommand command)
+    {
+        _onStart = command;
+    }
+
+    public void SetOnFinish(ICommand command)
+    {
+        _onFinish = command;
+    }
+
+    public void DoSomethingImportant()
+    {
+        Console.WriteLine("Invoker: Does anybody want something done before I begin?");
+        if (_onStart is ICommand)
+        {
+            _onStart.Execute();
+        }
+            
+        Console.WriteLine("Invoker: ...doing something really important...");
+            
+        Console.WriteLine("Invoker: Does anybody want something done after I finish?");
+        if (_onFinish is ICommand)
+        {
+            _onFinish.Execute();
+        }
+    }
+}
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        // The client code can parameterize an invoker with any commands.
+        Invoker invoker = new Invoker();
+        invoker.SetOnStart(new SimpleCommand("Say Hi!"));
+        Receiver receiver = new Receiver();
+        invoker.SetOnFinish(new ComplexCommand(receiver, "Send email", "Save report"));
+
+        invoker.DoSomethingImportant();
+    }
+}
---
+++ Command/RemoteControlDevice/Program.cs
@@ -0,0 +1,131 @@
+﻿//Command interface
+
+public interface ICommand
+{
+    void Execute();
+}
+
+public class TurnOnTVCommand : ICommand
+{
+    private TV _tv;
+
+    public TurnOnTVCommand(TV tv)
+    {
+        _tv = tv;
+    }
+
+    public void Execute()
+    {
+        _tv.TurnOn();
+    }
+}
+
+public class TurnOffTVCommand : ICommand
+{
+    private TV _tv;
+
+    public TurnOffTVCommand(TV tv)
+    {
+        _tv = tv;
+    }
+
+    public void Execute()
+    {
+        _tv.TurnOff();
+    }
+}
+
+public class IncreaseVolumeCommand : ICommand
+{
+    private TV _tv;
+    private int _volume;
+
+    public IncreaseVolumeCommand(TV tv, int volume)
+    {
+        _tv = tv;
+        _volume = volume;
+    }
+
+    public void Execute()
+    {
+        _tv.IncreaseVolume(_volume);
+    }
+}
+
+public class DecreaseVolumeCommand : ICommand
+{
+    private TV _tv;
+    private int _volume;
+
+    public DecreaseVolumeCommand(TV tv, int volume)
+    {
+        _tv = tv;
+        _volume = volume;
+    }
+
+    public void Execute()
+    {
+        _tv.DecreaseVolume(_volume);
+    }
+}
+
+// Receiver
+public class TV
+{
+    public void TurnOn()
+    {
+        Console.WriteLine("Turn on tv");
+    }
+
+    public void TurnOff()
+    {
+        Console.WriteLine("Turn off tv");
+    }
+
+    public void IncreaseVolume(int volumn)
+    {
+        Console.WriteLine($"Increase volumn to {volumn}");
+    }
+
+    public void DecreaseVolume(int volumn)
+    {
+        Console.WriteLine($"Decrease volumn to {volumn}");
+    }
+}
+
+// Invoker
+public class Remote
+{
+    private ICommand _command;
+
+    public void SetCommand(ICommand command)
+    {
+        _command = command;
+    }
+
+    public void PressButton()
+    {
+        _command.Execute();
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        TV tv = new TV();
+        Remote remote = new Remote();
+
+        remote.SetCommand(new TurnOnTVCommand(tv));
+        remote.PressButton();
+
+        remote.SetCommand(new IncreaseVolumeCommand(tv, 60));
+        remote.PressButton();
+
+        remote.SetCommand(new DecreaseVolumeCommand(tv, 10));
+        remote.PressButton();
+
+        remote.SetCommand(new TurnOffTVCommand(tv));
+        remote.PressButton();
+    }
+}
