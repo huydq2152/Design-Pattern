a66d4ffa20aeaceae8d5c8bfb0f4e77a8aee3d2b
huydq2152 <quanghuy2152@gmail.com>
2024-11-13 20:03:51 +0700

    add conceptual memento design pattern

---
+++ Memento/Conceptual/Program.cs
@@ -0,0 +1,174 @@
+﻿public class Originator
+    {
+        private string _state;
+
+        public Originator(string state)
+        {
+            _state = state;
+            Console.WriteLine("Originator: My initial state is: " + state);
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Originator: I'm doing something important.");
+            _state = GenerateRandomString(30);
+            Console.WriteLine($"Originator: and my state has changed to: {_state}");
+        }
+
+        private string GenerateRandomString(int length = 10)
+        {
+            var allowedSymbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var result = string.Empty;
+
+            while (length > 0)
+            {
+                result += allowedSymbols[new Random().Next(0, allowedSymbols.Length)];
+
+                Thread.Sleep(12);
+
+                length--;
+            }
+
+            return result;
+        }
+
+        public IMemento Save()
+        {
+            return new ConcreteMemento(_state);
+        }
+
+        public void Restore(IMemento memento)
+        {
+            if (memento is not ConcreteMemento)
+            {
+                throw new Exception("Unknown memento class " + memento);
+            }
+
+            _state = memento.GetState();
+            Console.Write($"Originator: My state has changed to: {_state}");
+        }
+    }
+
+    public interface IMemento
+    {
+        string GetName();
+
+        string GetState();
+
+        DateTime GetDate();
+    }
+
+    // The Concrete Memento contains the infrastructure for storing the
+    // Originator's state.
+    public class ConcreteMemento : IMemento
+    {
+        private readonly string _state;
+
+        private readonly DateTime _date;
+
+        public ConcreteMemento(string state)
+        {
+            _state = state;
+            _date = DateTime.Now;
+        }
+
+        // The Originator uses this method when restoring its state.
+        public string GetState()
+        {
+            return _state;
+        }
+        
+        // The rest of the methods are used by the Caretaker to display
+        // metadata.
+        public string GetName()
+        {
+            return $"{_date} / ({_state.Substring(0, 9)})...";
+        }
+
+        public DateTime GetDate()
+        {
+            return _date;
+        }
+    }
+
+    // The Caretaker doesn't depend on the Concrete Memento class. Therefore, it
+    // doesn't have access to the originator's state, stored inside the memento.
+    // It works with all mementos via the base Memento interface.
+    public class Caretaker
+    {
+        private List<IMemento> _mementos = new List<IMemento>();
+
+        private Originator _originator;
+
+        public Caretaker(Originator originator)
+        {
+            _originator = originator;
+        }
+
+        public void Backup()
+        {
+            Console.WriteLine("\nCaretaker: Saving Originator's state...");
+            _mementos.Add(_originator.Save());
+        }
+
+        public void Undo()
+        {
+            if (_mementos.Count == 0)
+            {
+                return;
+            }
+
+            var memento = _mementos.Last();
+            _mementos.Remove(memento);
+
+            Console.WriteLine("Caretaker: Restoring state to: " + memento.GetName());
+
+            try
+            {
+                _originator.Restore(memento);
+            }
+            catch (Exception)
+            {
+                Undo();
+            }
+        }
+
+        public void ShowHistory()
+        {
+            Console.WriteLine("Caretaker: Here's the list of mementos:");
+
+            foreach (var memento in _mementos)
+            {
+                Console.WriteLine(memento.GetName());
+            }
+        }
+    }
+    
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var originator = new Originator("Super-duper-super-puper-super.");
+            var caretaker = new Caretaker(originator);
+
+            caretaker.Backup();
+            originator.DoSomething();
+
+            caretaker.Backup();
+            originator.DoSomething();
+
+            caretaker.Backup();
+            originator.DoSomething();
+
+            Console.WriteLine();
+            caretaker.ShowHistory();
+
+            Console.WriteLine("\nClient: Now, let's rollback!\n");
+            caretaker.Undo();
+
+            Console.WriteLine("\n\nClient: Once more!\n");
+            caretaker.Undo();
+
+            Console.WriteLine();
+        }
+    }
