356b19bafcfb585c7798bf63784cf3b09f175480
huydq2152 <quanghuy2152@gmail.com>
2024-11-14 00:02:54 +0700

    add template method and visitor design pattern

---
+++ TemplateMethod/Conceptual/Program.cs
@@ -0,0 +1,95 @@
+﻿public abstract class AbstractClass
+{
+    public void TemplateMethod()
+    {
+        BaseOperation1();
+        RequiredOperations1();
+        BaseOperation2();
+        Hook1();
+        RequiredOperation2();
+        BaseOperation3();
+        Hook2();
+    }
+
+    // These operations already have implementations.
+    protected void BaseOperation1()
+    {
+        Console.WriteLine("AbstractClass says: I am doing the bulk of the work");
+    }
+
+    protected void BaseOperation2()
+    {
+        Console.WriteLine("AbstractClass says: But I let subclasses override some operations");
+    }
+
+    protected void BaseOperation3()
+    {
+        Console.WriteLine("AbstractClass says: But I am doing the bulk of the work anyway");
+    }
+
+    protected abstract void RequiredOperations1();
+
+    protected abstract void RequiredOperation2();
+
+    protected virtual void Hook1()
+    {
+    }
+
+    protected virtual void Hook2()
+    {
+    }
+}
+
+public class ConcreteClass1 : AbstractClass
+{
+    protected override void RequiredOperations1()
+    {
+        Console.WriteLine("ConcreteClass1 says: Implemented Operation1");
+    }
+
+    protected override void RequiredOperation2()
+    {
+        Console.WriteLine("ConcreteClass1 says: Implemented Operation2");
+    }
+}
+
+public class ConcreteClass2 : AbstractClass
+{
+    protected override void RequiredOperations1()
+    {
+        Console.WriteLine("ConcreteClass2 says: Implemented Operation1");
+    }
+
+    protected override void RequiredOperation2()
+    {
+        Console.WriteLine("ConcreteClass2 says: Implemented Operation2");
+    }
+
+    protected override void Hook1()
+    {
+        Console.WriteLine("ConcreteClass2 says: Overridden Hook1");
+    }
+}
+
+public class Client
+{
+    public static void ClientCode(AbstractClass abstractClass)
+    {
+        abstractClass.TemplateMethod();
+    }
+}
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Same client code can work with different subclasses:");
+
+        Client.ClientCode(new ConcreteClass1());
+
+        Console.Write("\n");
+
+        Console.WriteLine("Same client code can work with different subclasses:");
+        Client.ClientCode(new ConcreteClass2());
+    }
+}
---
+++ Visitor/XmlSample/Program.cs
@@ -0,0 +1,100 @@
+﻿public interface IXmlNode
+{
+    void Accept(IXmlVisitor visitor);
+}
+
+public class XmlElement : IXmlNode
+{
+    public string Name { get; set; }
+    public string Text { get; set; }
+    public List<IXmlNode> Children { get; set; } = new List<IXmlNode>();
+
+    public void Accept(IXmlVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+}
+
+public class XmlAttribute : IXmlNode
+{
+    public string Name { get; set; }
+    public string Value { get; set; }
+
+    public void Accept(IXmlVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+}
+
+
+public interface IXmlVisitor
+{
+    void Visit(XmlElement element);
+    void Visit(XmlAttribute attribute);
+}
+
+public class XmlPrintingVisitor : IXmlVisitor
+{
+    public void Visit(XmlElement element)
+    {
+        Console.WriteLine($"<{element.Name}>");
+        foreach (var child in element.Children)
+        {
+            child.Accept(this);
+        }
+        Console.WriteLine($"</{element.Name}>");
+    }
+
+    public void Visit(XmlAttribute attribute)
+    {
+        Console.WriteLine($" {attribute.Name}=\"{attribute.Value}\"");
+    }
+}
+
+public class XmlSearchingVisitor : IXmlVisitor
+{
+    private readonly string _targetName;
+
+    public XmlSearchingVisitor(string targetName)
+    {
+        _targetName = targetName;
+    }
+
+    public void Visit(XmlElement element)
+    {
+        if (element.Name == _targetName)
+        {
+            Console.WriteLine($"Found element: {element.Name}");
+        }
+        foreach (var child in element.Children)
+        {
+            child.Accept(this);
+        }
+    }
+
+    public void Visit(XmlAttribute attribute)
+    {
+        if (attribute.Name == _targetName)
+        {
+            Console.WriteLine($"Found attribute: {attribute.Name}");
+        }
+    }
+}
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var root = new XmlElement { Name = "root" };
+        root.Children.Add(new XmlAttribute { Name = "id", Value = "1" });
+        root.Children.Add(new XmlElement { Name = "child", Text = "text" });
+        
+        var printingVisitor = new XmlPrintingVisitor();
+        root.Accept(printingVisitor);
+
+        Console.WriteLine();
+        
+        var searchingVisitor = new XmlSearchingVisitor("child");
+        root.Accept(searchingVisitor);
+    }
+}
